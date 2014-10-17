9dca5441872dc16a83b9023447013d101330c958
Teno <Torkinvon@web.de>
2014-10-17 12:50:13 +0200

    16.10

--- forms/login.php
+++ forms/login.php
@@ -1,30 +1,26 @@
-<<<<<<< HEAD
-<html>
-<body>
-<h1>Anmelden</h1>
-<p>bitte hier anmelden</p>
-<form method="POST" action="<?php echo $this->url(array('controller'=>'auth','action'=>'identify')); ?>">
-
-	<div>
-		<label>Username</label>
-		<input type="text" name="username" value="" />
-	</div>
-	<div>
-		<label>passwort</label>
-		<input type="password" name="password" value="" />
-	</div>
-	<div>
-		<input type="submit" name="login" value="Login" />	
-	</div>
-</form>
-</body>
-</html>
-=======
-<h1>Anmelden</h1>
-<p>bitte hier anmelden</p>
-<?php echo $this->formResponse; ?>
-<?php echo $this->form; ?>
 <?php 
-
-
->>>>>>> c8fe6c8aaf075944b6e10d63d9e9a697a7057997
+class From_Login extends Zend_Form{
+	
+	public function init(){
+		$required = array('massages'=>'Eingabe erforderlich');
+		
+		
+		$name = $this->createElement('text','name');
+		$name
+			->setLabel('name')
+			->setRequired(true)
+			->setValidator('notEmpty',true, $required);
+		$this->addElement($name);
+		
+		$password = $this->createElement('text','password');
+		$password
+			->setLabel('password')
+			->setRequired(true)
+			->setValidator('notEmpty',true, $required);
+		$this->addElement($password);
+		
+		$submit = $this->createElement('submit', 'submit');
+		$submit->setLabel('Anmelden');
+		$this->addElement($submit);
+	}	
+}
